092a2fed3ae9fcb85b9467c9649b244c96ca70a0
moerishiraishi <moeri_0408@yahoo.co.jp>
2016-12-09 01:35:59 +0900

    add column to messages table

--- db/migrate/20161208163107_create_messages.rb
+++ db/migrate/20161208163107_create_messages.rb
@@ -1,8 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
-      t.timestamps
+    t.text :body, null: false
+    t.string :image
+    t.references :user_id, null: false
+    t.references :group_id, null: false
+    t.timestamps
     end
   end
 end
