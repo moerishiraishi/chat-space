38158be3e2703c6d7d6efe43a7575686ad1eeb8d
moerishiraishi <moeri_0408@yahoo.co.jp>
2016-12-09 15:50:54 +0900

    change messages_controller
    index action
    create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,29 @@
 class MessagesController < ApplicationController
+  def index
+    @groups = Group.all
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @message = Message.new
+  end
+
   def new
   end
 
   def create
-    
+    @message = current_user.messages.new(create_params)
+    if @message.save
+      flash[:notice] = "メッセージを送信しました"
+    else
+      flash[:alert] = "メッセージを送信できませんでした"
+    end
+    redirect_to group_messages_path
+  end
+
+
+  private
+
+  def create_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
 end
+
