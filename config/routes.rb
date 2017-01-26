b2136730c48db247b006ef9b7af6b78ba7d23a22
moerishiraishi <moeri_0408@yahoo.co.jp>
2017-01-26 19:39:37 +0900

    add search action to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
+  resources :users do
+    collection { get 'search' }
+  end
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :new, :create]
   end
