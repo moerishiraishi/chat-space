5717e6e00bd4b3ab0f637897ad92b2c1e53fd000
moerishiraishi <moeri_0408@yahoo.co.jp>
2017-01-26 19:39:06 +0900

    define search action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def edit
   end
+
+  def search
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").order('name ASC')
+    respond_to do |format|
+      format.any
+      format.json { render json: @users }
+    end
+  end
 end
