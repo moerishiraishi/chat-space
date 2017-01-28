7c032cdf2456eb74691782202fe7ecd1f278aadb
moerishiraishi <moeri_0408@yahoo.co.jp>
2017-01-29 01:52:04 +0900

    change user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,6 @@
 $(function() {
+
+  //インクリメンタルサーチで検索したユーザーの表示機能
   function BuildHTML(user) {
       var html = '<div class="chat-group-user clearfix chat-group-user-add">' +
                    '<p class="chat-group-user__name">' + user.name + '</p>' +
@@ -8,6 +10,7 @@ $(function() {
       return html;
   }
 
+  //追加するユーザーの候補の表示機能
   function AddUserHTML(id, name) {
     var html = '<div class="chat-group-user clearfix" id="chat-group-user-' + id + '">' +
                  '<input type="hidden" name="group[user_ids][]" value="' + id + '">' +
@@ -18,40 +21,45 @@ $(function() {
     return html;
   }
 
-  $('#user-search-field').on('keyup', function() {
-    function ajaxSearch(text) {
-      $.ajax({
-        type: "GET",
-        url: "/users/search",
-        data: {
-          user: {
-            text: $('#user-search-field').val()
-          }
-        },
-        dataType: 'json'
-      })
-      .done(function(data) {
-        var insertHtml = ""
-        $.each(data, function(i, user) {
-          insertHtml += BuildHTML(user);
-        });
-        $('#user-search-result').html(insertHtml);
-      })
-      .fail(function() {
-        alert('error');
+  //インクリメンタルサーチ実行機能
+  function ajaxSearch(text) {
+    $.ajax({
+      type: "GET",
+      url: "/users/search",
+      data: {
+        user: {
+          text: $('#user-search-field').val()
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var insertHtml = ""
+      $.each(data, function(i, user) {
+        insertHtml += BuildHTML(user);
       });
-    };
+      $('#user-search-result').html(insertHtml);
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  //インクリメンタルサーチ実行
+  $('#user-search-field').on('keyup', function() {
     ajaxSearch();
   });
 
+  //インクリメンタルサーチで検索したユーザーの表示
   $(document).on('click', '.chat-group-user__btn--add', function(){
     var id = $(this).data('user-id');
     var name = $(this).data('user-name');
     $(this).parent().remove();
-    var html = AddUserHTML(id, name)
-    $('#chat-group-users').append(html);
-   });
+    var insertHtml = AddUserHTML(id, name)
+    $('#chat-group-users').append(insertHtml);
+  });
 
+    //追加するユーザーの候補の表示
    $(document).on('click', '.chat-group-user__btn--remove', function(){
     $(this).parent().remove();
    });
